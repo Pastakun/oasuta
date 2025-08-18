9834c42c166bfdc54cc8d92b7abb376e2ce1ef23
Pastakun <130649332+Pastakun@users.noreply.github.com>
2025-08-18 17:50:05 +0900

    Update index.js

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ client.on("messageCreate", async (message) => {
         const response = await modelclient.path("/chat/completions").post({
           body: {
             messages: [
-              { role: "system", content: `Fill "..." in Discord.js v14 code ` + userdata.system },
+              { role: "system", content: `Fill "..." in Discord.js v13 code ` + userdata.system },
               {
                 role: "user",
                 content: `if(message.content==='${prompt}'){...}`,
@@ -151,7 +151,7 @@ client.on("interactionCreate", async (interaction) => {
         const response = await modelclient.path("/chat/completions").post({
           body: {
             messages: [
-              { role: "system", content: `Fill "..." in Discord.js v14 code ` + userdata.system },
+              { role: "system", content: `Fill "..." in Discord.js v13 code ` + userdata.system },
               {
                 role: "user",
                 content: `if(interaction.commandName==='chat'){await interaction.deferReply();if(interaction.options.getString("content")==='${prompt}'){...}}`,
